f8f3cb801a6fff74755f5b8b0749ffb3256f10f2
Eyad Kobatte <ekobatte96@gmail.com>
2020-04-19 15:43:26 +0530

    add card

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # npm-business-card-eyadkobatte
-Personal Business Card
+
+Display a business card with my details. [Inspired by Max Stoiber from this tweet](https://twitter.com/mxstbr/status/1200358955811295233)
+
+The goal for this is to try and not use any dependencies other than devDependencies like eslint and prettier.
+
+It would be a personal goal to have 0 dependencies in the end.
---
+++ index.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+const resetTerminalColor = '\x1b[0m';
+const fgColor = '\x1b[34m';
+const bgColor = '\x1b[47m';
+
+const log = (message) => {
+  console.log(`${fgColor}${bgColor}%s`, message, resetTerminalColor);
+};
+
+console.log('\n');
+log('                                                        ');
+log(' ------------------------------------------------------ ');
+log('|  Eyad Kobatte / eyadkobatte.com                      |');
+log('|                                                      |');
+log('|  Twitter:  https://twitter.com/EyadKobatte           |');
+log('|  GitHub:   https://github.com/eyadkobatte            |');
+log('|  LinkedIn: https://www.linkedin.com/in/eyadkobatte/  |');
+log('|  Website:  https://eyadkobatte.com                   |');
+log('|  Blog:     https://blog.eyadkobatte.com              |');
+log(' ------------------------------------------------------ ');
+log('                                                        ');
+console.log('\n');
